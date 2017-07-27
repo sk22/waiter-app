fa11444796d367ec6cad9a0be3f34712372e25a0
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-27 15:36:52 +0200

    Add example state

---
+++ src/exampleState.js
@@ -0,0 +1,35 @@
+export default {
+  scenarios: {
+    s1: {
+      name: 'Festival',
+      products: {
+        p1: 3.5,
+        p2: 7
+      }
+    }
+  },
+  products: {
+    p1: {
+      name: 'Drink',
+      category: 'Beverages'
+    },
+    p2: {
+      name: 'Meal',
+      category: 'Food'
+    }
+  },
+  orders: {
+    o1: {
+      scenario: 's1',
+      delivered: false,
+      attributes: {
+        place: '13',
+        note: ''
+      },
+      products: {
+        p1: 4,
+        p2: 3
+      }
+    }
+  }
+}
