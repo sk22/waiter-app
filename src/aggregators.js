e7cda3d478befc7c5da63fe473a556bfb38e3d07
Samuel Kaiser <samuel.kaiser01@gmail.com>
2017-07-30 23:23:52 +0200

    Improve categoryProducts

--- src/aggregators.js
+++ src/aggregators.js
@@ -2,7 +2,7 @@ export const categoryProducts = products => {
   const categories = new Set(
     Object.values(products).map(product => product.category)
   )
-  return [...categories, ''].reduce(
+  return Array.from(categories).reduce(
     (object, category) => ({
       ...object,
       [category]: Object.keys(products).filter(
